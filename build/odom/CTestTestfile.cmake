be70fd8198fe3fc463f5ce0b89553db849ddacaf
HardWorkingDavid <18811361762@163.com>
2024-03-02 11:40:48 +0800

    pub_sub learning

--- build/odom/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bigdavid/视频/ROS_learning/src/odom
-# Build directory: /home/bigdavid/视频/ROS_learning/build/odom
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/pub_sub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bigdavid/视频/ROS_learning/src/pub_sub
-# Build directory: /home/bigdavid/视频/ROS_learning/build/pub_sub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
